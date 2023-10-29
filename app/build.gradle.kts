8a9dcaa5265dd80d598a9f39b2b7356ab7bad3cc
hassisto4 <mariodigirolamowork@gmail.com>
2023-10-29 08:18:16 +0100

    feat(Firebase): setup firebase
    Add song link insertion field to main UI.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,20 +51,27 @@ android {
 }
 
 dependencies {
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    // Compose
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    // Test
     testImplementation("junit:junit:4.13.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- app/src/main/java/com/mdg/incognitune/ui/IncognituneApp.kt
+++ app/src/main/java/com/mdg/incognitune/ui/IncognituneApp.kt
@@ -9,12 +9,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mdg.incognitune.ui.theme.IncognituneTheme
@@ -53,22 +60,18 @@ fun DailySuggestedSongCard(
         ) {
             Text(
                 text = "Suggested song",
-                style = Typography.bodyMedium
+                style = Typography.titleLarge
             )
             Spacer(modifier = Modifier.size(14.dp))
             Text(
-                text = "This will be the Spotify link",
-                style = Typography.bodyMedium
-            )
-            Spacer(modifier = Modifier.size(14.dp))
-            Text(
-                text = "This will be the YouTube link",
+                text = "This will show the fantastical song link!",
                 style = Typography.bodyMedium
             )
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun YourDailySubmissionCard(
     modifier: Modifier = Modifier
@@ -81,6 +84,15 @@ fun YourDailySubmissionCard(
             Text(
                 text = "Paste your song link down there!",
                 style = Typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.size(14.dp))
+            // TODO: this belong to a viewModel
+            var yourSongLink by rememberSaveable { mutableStateOf("") }
+
+            OutlinedTextField(
+                value = yourSongLink,
+                onValueChange = { yourSongLink = it },
+                label = { Text("Your song link") }
             )
             Spacer(modifier = Modifier.size(14.dp))
             Button(onClick = {}) {
